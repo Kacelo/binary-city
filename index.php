756ce3428e0675d5b06c7d39bdd462e050f298de
Kacelo <vernonkachelo@gmail.com>
2024-11-25 11:26:26 +0200

    added join table script

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- <?php ?> -->
     <h1>Sign Up</h1>
     <form action="includes/clients.inc.php" method="post">
-        <label for="username">Username:</label>
+        <label for="username">Client Name:</label>
         <input type="text" id="username" name="client_name" placeholder="Enter your username" required><br><br>
         <button type="submit" name="submit">Sign Up</button>
     </form>
